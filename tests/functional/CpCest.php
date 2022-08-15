523e0d50de6c81e6ba39b69b9cce43f43a9cff64
Alban Jubert <ajubert@simplonprod.co>
2022-08-15 15:43:24 +0200

    test: Update Cp functional test for Craft 4.2 compliance

--- tests/functional/CpCest.php
+++ tests/functional/CpCest.php
@@ -86,7 +86,7 @@ class CpCest
             ],
         ]);
         $I->seeResponseCodeIs(200);
-        $I->see('Plugin settings saved');
+        $I->seeCurrentUrlMatches('/admin\/settings$/');
         $I->seeInDatabase('projectconfig', ['path' => 'plugins.google-recaptcha.settings.version', 'value' => '"3"']);
     }
 }
